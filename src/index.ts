3b735fc103e9c7c88d4173c28a8f006914bab0d2
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-07-26 13:34:26 -0300

    fix: changed path verification
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -8,24 +8,14 @@ class PynieDB implements IPynieDB {
 	fullPath: string = '';
 
 	connect (dbName: string, options: IConnectOptions) {
-		let dbPath: string = '';
 		let fullPath: string = '';
 
-		if (options.path.includes('/')) {
-			let pathArray = options.path.split('/');
-			if (pathArray[0] === '' && pathArray[pathArray.length - 1] === '') dbPath = `${__dirname}${options.path}`;
-			if (pathArray[0] === '' && pathArray[pathArray.length - 1] !== '') dbPath = `${__dirname}${options.path}/`;
-			if (pathArray[0] !== '' && pathArray[pathArray.length - 1] === '') dbPath = `${__dirname}/${options.path}`;
-			if (pathArray[0] !== '' && pathArray[pathArray.length - 1] !== '') dbPath = `${__dirname}/${options.path}/`;
-		}
-		else dbPath = `${__dirname}/${options.path}/${dbName}`;
-
-		fullPath = path.normalize(dbPath);
+		fullPath = path.normalize(options.path);
 		this.fullPath = `${fullPath}${dbName}`;
 
 		if (!fs.existsSync(fullPath)) {
 			fs.mkdirSync(fullPath);
-			fs.mkdirSync(`${fullPath}${dbName}`);
+			if (fs.existsSync(fullPath)) fs.mkdirSync(`${fullPath}${dbName}`);
 			console.log(`\ndatabase '${dbName}' created\n`);
 		}
 	}
