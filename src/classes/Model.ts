07e805641a6bb7f2fe81c06f3f9e0f8c0d0848fa
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-07-28 17:24:42 -0300

    feat: added schema relations
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/classes/Model.ts
+++ src/classes/Model.ts
@@ -4,39 +4,68 @@ import fsPromise from 'fs/promises';
 
 import Schema from './Schema';
 
-class Model implements ModelClass{
+class Model implements ModelClass {
 	model: string;
 	schema: Schema;
 	private path: string = '';
 
-	constructor (model: string, schema: Schema) {
+	constructor(model: string, schema: Schema) {
 		this.model = model;
 		this.schema = schema;
 	}
 
-	async createTable (): Promise<any> {
+	async createTable(): Promise<any> {
 		try {
-			const path = `${this.path}/${this.model}.json`;
-			if (!fs.existsSync(path)) await fsPromise.writeFile(path, '[]');
+			const schema = this.schema.schema;
+			const modelsWithRelation: string[] = [];
+			Object.keys(schema).map((obj) => {
+				let schemaRelation = schema[obj].relation?.modelName;
+				if (typeof schemaRelation === 'string') {
+					modelsWithRelation.push(schemaRelation)
+				}
+			});
+			modelsWithRelation.push(this.model);
+			if (modelsWithRelation.length > 0) {
+				modelsWithRelation.map(async (model) => {
+					let path = `${this.path}/${model}.json`;
+					if (!fs.existsSync(path)) await fsPromise.writeFile(path, '[]');
+				})
+			} else {
+				const path = `${this.path}/${this.model}.json`;
+				if (!fs.existsSync(path)) await fsPromise.writeFile(path, '[]');
+			}
+
 		} catch (error) {
 			return Promise.reject(error);
 		}
 	}
 
-	async create (data: any): Promise<object> {
+	async create(data: any): Promise<object> {
 		try {
 			const schema = this.schema.schema;
-			const schemaStructure: any = { id: 1, ...data};
+			const schemaStructure: any = { id: 1, ...data };
 			const allData: Array<any> = await this.findAll();
+			const error: any = {};
 
-			Object.keys(schema).map((obj) => {
+			Object.keys(schema).map(async (obj) => {
 				let dataType = typeof data[obj];
 				let schemaType = schema[obj].type;
 				let dataObjKeys = Object.keys(data);
 				let schemaDefaultValue = schema[obj].default;
+				let schemaRelation = schema[obj].relation?.modelName;
 
-				if (!dataObjKeys.includes(obj) && typeof schemaDefaultValue == 'undefined') throw new Error(`Expected param '${obj}'`);
-				if (dataType != schemaType && typeof schemaDefaultValue == 'undefined') throw new Error(`the type '${dataType}' is not expected. Expected is: ${schemaType}`);
+				if (!dataObjKeys.includes(obj) && typeof schemaDefaultValue == 'undefined') error.message = `Expected param '${obj}'`;
+				if (dataType != schemaType && typeof schemaDefaultValue == 'undefined') error.message = `the type '${dataType}' is not expected. Expected is: ${schemaType}`;
+				if (typeof schemaRelation != 'undefined' && typeof schemaDefaultValue != 'undefined') error.message = `Not expected a default value to relation at Schema '${this.model}'`;
+
+				if (schemaRelation) {
+					var schemaParam = schemaStructure[obj];
+					var relation = JSON.parse(await fsPromise.readFile(`${this.path}/${schemaRelation}.json`, { encoding: 'utf8' }));
+					var relFind = relation.find((res: any) => res.id == schemaParam);
+
+					if (!relFind) error.message = `Cannot find schema ${schemaRelation} with [id: ${schemaParam}]`;
+
+				}
 
 				if (typeof schemaDefaultValue != 'undefined') {
 					schemaStructure[obj] = schemaDefaultValue;
@@ -44,7 +73,10 @@ class Model implements ModelClass{
 						if (!schema[dataObj]) delete schemaStructure[dataObj];
 					});
 				}
+
 			});
+
+			if (error.message) throw new Error(error.message)
 
 			if (allData.length <= 0) {
 				await fsPromise.writeFile(`${this.path}/${this.model}.json`, JSON.stringify([schemaStructure]));
@@ -55,17 +87,29 @@ class Model implements ModelClass{
 			}
 
 			return schemaStructure;
-		} catch(error) {
+		} catch (error) {
 			return Promise.reject(error);
 		}
 	}
 
 	async findAll(): Promise<Array<any>> {
 		try {
-			const response = await fsPromise.readFile(`${this.path}/${this.model}.json`, { encoding: 'utf8' });
+			const schema = this.schema.schema;
+			const response = JSON.parse(await fsPromise.readFile(`${this.path}/${this.model}.json`, { encoding: 'utf8' }));
+			var newResponse: any[] = [];
+			Object.keys(schema).map((obj) => {
+				let schemaRelation = schema[obj].relation?.modelName;
+				if (schemaRelation) {
+					let relationModel = JSON.parse(fs.readFileSync(`${this.path}/${schemaRelation}.json`, { encoding: 'utf8' }));
+					response.map((res: any) => {
+						return res[obj] = relationModel.find((rel: any) => rel.id == res[obj]);
+					})
+					newResponse = response;
+				}
+			});
 
-			return JSON.parse(response);
-		} catch(error) {
+			return newResponse.length > 0 ? newResponse : response;
+		} catch (error) {
 			return Promise.reject(error);
 		}
 	}
@@ -75,7 +119,7 @@ class Model implements ModelClass{
 			const allData = await this.findAll();
 
 			return allData.find(data => data[param] == value);
-		} catch(error) {
+		} catch (error) {
 			return Promise.reject(error);
 		}
 	}
@@ -85,7 +129,7 @@ class Model implements ModelClass{
 			const allData = await this.findAll();
 			const newData = allData.filter(data => data[param] != value);
 			await fsPromise.writeFile(`${this.path}/${this.model}.json`, JSON.stringify(newData));
-		} catch(error) {
+		} catch (error) {
 			return Promise.reject(error);
 		}
 	}
